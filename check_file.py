7c20775b14bc27b1755c7dc098e3dc2d73c956e8
bugbare <rshah@bugbare.co.uk>
2024-03-14 21:46:35 +0000

    updated python file to meet lint issues

--- check_file.py
+++ check_file.py
@@ -1,11 +1,14 @@
-"""Module providing a function printing python version."""
-import torch
+"""Module providing a function printing python version and cuda"""
 import sys
+import torch
+
 
 def print_python_version():
+    """Function printing python version."""
     print(f"\npython version is {sys.version}\n")
     
 def print_cuda_status():
+    """Function printing cuda status."""
     x = torch.rand(5, 3)
     print(f"\n{x}")
     print(f'\nIs Cuda available ---> {torch.cuda.is_available()}')
